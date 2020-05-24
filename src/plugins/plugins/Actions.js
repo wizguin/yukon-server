437d51ddbc838fcff30e3fc5087c0a8839216943
wizguin <16276645+wizguin@users.noreply.github.com>
2020-05-24 21:17:54 +0100

    Added actions plugin

---
+++ src/plugins/plugins/Actions.js
@@ -0,0 +1,25 @@
+import Plugin from '../Plugin'
+
+
+export default class Actions extends Plugin {
+
+    constructor(users, rooms) {
+        super(users, rooms)
+        this.events = {
+            'send_position': this.sendPosition,
+            'send_frame': this.sendFrame
+        }
+    }
+
+    sendPosition(args, user) {
+        user.x = args.x
+        user.y = args.y
+        this.sendRoom(user, 'send_position', { id: user.data.id, x: args.x, y: args.y })
+    }
+
+    sendFrame(args, user) {
+        if (args.loop) user.frame = args.frame
+        this.sendRoom(user, 'send_frame', { id: user.data.id, frame: args.frame, loop: args.loop })
+    }
+
+}
