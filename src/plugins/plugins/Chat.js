03f34222800ada9eb3372dd6272eae2ec60e0e08
wizguin <16276645+wizguin@users.noreply.github.com>
2021-01-26 13:52:34 +0000

    Added emotes

--- src/plugins/plugins/Chat.js
+++ src/plugins/plugins/Chat.js
@@ -6,13 +6,18 @@ export default class Chat extends Plugin {
     constructor(users, rooms) {
         super(users, rooms)
         this.events = {
-            'send_message': this.sendMessage
+            'send_message': this.sendMessage,
+            'send_emote': this.sendEmote
         }
     }
 
     sendMessage(args, user) {
         // Todo: message verification
         user.room.send(user, 'send_message', { id: user.data.id, message: args.message })
+    }
+
+    sendEmote(args, user) {
+        user.room.send(user, 'send_emote', { id: user.data.id, emote: args.emote })
     }
 
 }
