96aa871196023e76cd9cd36a24f0da3fb0400e68
wizguin <16276645+wizguin@users.noreply.github.com>
2021-02-10 15:46:30 +0000

    Renamed get_penguin for consistency

--- src/plugins/plugins/Get.js
+++ src/plugins/plugins/Get.js
@@ -6,16 +6,16 @@ export default class Get extends Plugin {
     constructor(users, rooms) {
         super(users, rooms)
         this.events = {
-            'get_penguin': this.getPenguin
+            'get_player': this.getPlayer
         }
     }
 
-    async getPenguin(args, user) {
+    async getPlayer(args, user) {
         let userData = await this.db.getUserById(args.id)
         let { banned, loginKey, password, rank, ...penguin } = userData.dataValues
 
         if (userData) {
-            user.send('get_penguin', { penguin: penguin, showCard: args.showCard })
+            user.send('get_player', { penguin: penguin, showCard: args.showCard })
         }
     }
 
