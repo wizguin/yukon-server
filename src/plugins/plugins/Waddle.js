9af1f8bbbb15f6ae0b62a675bd4fe6b704539cfa
wizguin <16276645+wizguin@users.noreply.github.com>
2022-01-02 17:23:02 +0000

    Update waddle

--- src/plugins/plugins/Waddle.js
+++ src/plugins/plugins/Waddle.js
@@ -13,27 +13,27 @@ export default class Waddle extends Plugin {
     }
 
     getWaddles(args, user) {
-        let waddles = Object.fromEntries(Object.values(user.room.waddles).map(waddle => {
-            let users = waddle.users.map(user => user ? user.data.username : null)
+        // let waddles = Object.fromEntries(Object.values(user.room.waddles).map(waddle => {
+        //     let users = waddle.users.map(user => user ? user.data.username : null)
 
-            return [waddle.id, users]
-        }))
+        //     return [waddle.id, users]
+        // }))
 
-        user.send('get_waddles', { waddles: waddles })
+        // user.send('get_waddles', { waddles: waddles })
     }
 
     joinWaddle(args, user) {
-        let waddle = user.room.waddles[args.id]
+        // let waddle = user.room.waddles[args.id]
 
-        if (waddle && waddle.users.includes(null) && !user.waddle) {
-            waddle.add(user)
-        }
+        // if (waddle && waddle.users.includes(null) && !user.waddle) {
+        //     waddle.add(user)
+        // }
     }
 
     leaveWaddle(args, user) {
-        if (user.waddle) {
-            user.waddle.remove(user)
-        }
+        // if (user.waddle) {
+        //     user.waddle.remove(user)
+        // }
     }
 
 }
