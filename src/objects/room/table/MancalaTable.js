f2e7f4dee29a5b85ec9bee3964feda903eedb263
wizguin <16276645+wizguin@users.noreply.github.com>
2023-02-27 19:14:35 +0000

    mancala: send_move handling

--- src/objects/room/table/MancalaTable.js
+++ src/objects/room/table/MancalaTable.js
@@ -12,4 +12,121 @@ export default class MancalaTable extends BaseTable {
         ]
     }
 
+    sendMove(args, user) {
+        if (!this.started) {
+            return
+        }
+
+        let hole = args.hole
+
+        if (!this.isValidMove(hole)) {
+            return
+        }
+
+        let move = this.makeMove(hole)
+        this.send('send_move', { turn: this.currentTurn, hole: hole, move: move })
+
+        if (this.isGameOver()) {
+            // reset table here
+        }
+
+        if (move !== 'free') {
+            this.currentTurn = (this.currentTurn === 1) ? 2 : 1
+        }
+    }
+
+    isValidMove(hole) {
+        if (this.map[hole] <= 0) {
+            return false
+        }
+
+        if (this.currentTurn === 1 && this.isTurn1Side(hole)) {
+            return true
+        }
+
+        if (this.currentTurn === 2 && this.isTurn2Side(hole)) {
+            return true
+        }
+
+        return false
+    }
+
+    makeMove(hole) {
+        let stones = this.map[hole]
+
+        this.map[hole] = 0
+
+        while (stones > 0) {
+            hole = this.getNextHole(hole)
+
+            this.map[hole]++
+            stones--
+        }
+
+        return this.checkLastHole(hole)
+    }
+
+    getNextHole(hole) {
+        hole++
+        let opponentMancala = (this.currentTurn === 1) ? 13 : 6
+
+        if (hole === opponentMancala) {
+            hole++
+        }
+
+        if (hole > this.map.length - 1) {
+            hole = 0
+        }
+
+        return hole
+    }
+
+    checkLastHole(hole) {
+        // Capture
+        let oppositeHole = 12 - hole
+        let myMancala = (this.currentTurn === 1) ? 6 : 13
+
+        if (this.map[hole] === 1 && this.map[oppositeHole] > 0) {
+            // Only if on your side
+            if (this.currentTurn === 1 && this.isTurn1Side(hole) || this.currentTurn === 2 && this.isTurn2Side(hole)) {
+                this.map[myMancala] += this.map[oppositeHole] + 1
+                this.map[hole] = 0
+                this.map[oppositeHole] = 0
+
+                return 'capture'
+            }
+        }
+
+        // Free turn
+        if (this.currentTurn === 1 && hole === myMancala || this.currentTurn === 2 && hole === myMancala) {
+            return 'free'
+        }
+
+        return ''
+    }
+
+    isGameOver() {
+        // Sums not including mancalas
+        let player1Sum = this.sum(this.map.slice(0, 6))
+        let player2Sum = this.sum(this.map.slice(7, -1))
+
+        if (player1Sum === 0 || player2Sum === 0) {
+            return true
+        }
+    }
+
+    isTurn1Side(hole) {
+        return hole >= 0 && hole <= 5
+    }
+
+    isTurn2Side(hole) {
+        return hole >= 7 && hole <= 12
+    }
+
+    sum(array) {
+        return array.reduce((previousValue, currentValue) => {
+            return previousValue + currentValue
+        }, 0)
+    }
+
 }
