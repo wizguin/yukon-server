802c1668d08a5dffb4149eb24f1da6a84a8a0330
wizguin <16276645+wizguin@users.noreply.github.com>
2023-02-27 18:39:39 +0000

    mancala: Add test map

--- src/objects/room/table/MancalaTable.js
+++ src/objects/room/table/MancalaTable.js
@@ -3,5 +3,13 @@ import BaseTable from './BaseTable'
 
 export default class MancalaTable extends BaseTable {
 
+    init() {
+        super.init()
+
+        this.map = [
+            4, 4, 4, 4, 4, 4, 0,
+            4, 4, 4, 4, 4, 4, 0
+        ]
+    }
 
 }
