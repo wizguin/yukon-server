072ee9eb6e8638d883b2e1fc371cab481264828d
wizguin <16276645+wizguin@users.noreply.github.com>
2023-03-25 20:02:12 +0000

    cardjitsu: Add start

--- src/objects/instance/card/CardInstance.js
+++ src/objects/instance/card/CardInstance.js
@@ -9,4 +9,17 @@ export default class CardInstance extends BaseInstance {
         this.id = 998
     }
 
+    start() {
+        let users = this.users.filter(Boolean).map(user => {
+            return {
+                username: user.username,
+                color: user.color
+            }
+        })
+
+        this.send('start_game', { users: users })
+
+        super.start()
+    }
+
 }
