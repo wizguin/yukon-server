9cc07f0132957d2aa9614d3fef7d5b7c8ecb3614
wizguin <16276645+wizguin@users.noreply.github.com>
2023-04-13 13:06:42 +0100

    sensei: Match making matching and tick

--- src/objects/room/matchmaker/CardMatchmaker.js
+++ src/objects/room/matchmaker/CardMatchmaker.js
@@ -1,3 +1,6 @@
+import MatchmakerPlayer from './MatchmakerPlayer'
+
+
 export default class CardMatchmaker {
 
     constructor(matchmaker, room) {
@@ -5,21 +8,71 @@ export default class CardMatchmaker {
 
         this.room = room
 
-        this.users = []
+        this.maxPlayers = 2
+        this.matchEvery = 10
+
+        this.players = {}
+
+        this.start()
+    }
+
+    start() {
+        setInterval(this.tick.bind(this), 1000)
+    }
+
+    tick() {
+        let values = Object.values(this.players)
+
+        let matchesLength = values.length - values.length % this.maxPlayers
+
+        if (!matchesLength) return
+
+        let matches = values.filter((p, i) => i < matchesLength)
+
+        for (let i = 0; i < matchesLength; i += this.maxPlayers) {
+            let matched = matches.slice(i, i + this.maxPlayers)
+
+            this.updateMatched(matched)
+        }
+    }
+
+    updateMatched(matched) {
+        let ready = matched.some(player => player.tick == -1)
+
+        if (!ready) {
+            this.onTick(matched)
+            this.decreaseTick(matched)
+
+            return
+        }
+    }
+
+    onTick(matched) {
+        let users = matched.map(player => player.user.username)
+
+        for (let player of matched) {
+            player.send('tick_matchmaking', { tick: player.tick, users: users })
+        }
+    }
+
+    decreaseTick(matched) {
+        for (let player of matched) {
+            player.tick -= 1
+        }
     }
 
     add(user) {
-        this.users.push(user)
+        this.players[user.id] = new MatchmakerPlayer(user, this.matchEvery)
 
         user.send('join_matchmaking')
     }
 
     remove(user) {
-        this.users = this.users.filter(u => u != user)
+        delete this.players[user.id]
     }
 
     includes(user) {
-        return this.users.includes(user)
+        return user.id in this.players
     }
 
 }
