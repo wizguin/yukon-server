6ac8767fd81610d37a9c7cf45ed8970e6788f0d4
wizguin <16276645+wizguin@users.noreply.github.com>
2023-08-30 00:09:51 +0100

    cardjitsu: Updated CardCollection

--- src/database/collections/CardCollection.js
+++ src/database/collections/CardCollection.js
@@ -7,4 +7,29 @@ export default class CardCollection extends Collection {
         super(user, models, 'cards', 'cardId')
     }
 
+    // Owned cards * their quantities
+    get deck() {
+        let deck = []
+
+        Object.keys(this.collection).forEach(card => {
+            const quantity = this.getQuantity(card)
+            deck.push(...Array(quantity).fill(card))
+        })
+
+        return deck
+    }
+
+    getQuantity(card) {
+        return this.collection[card].quantity
+    }
+
+    add(card) {
+        if (this.includes(card)) {
+            this.collection[card].increment({ quantity: 1 })
+
+        } else {
+            super.add({ userId: this.user.id, cardId: card, quantity: 1, memberQuantity: 0 })
+        }
+    }
+
 }
