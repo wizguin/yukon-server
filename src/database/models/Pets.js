db83ef29a0159ef275a2235b0e44d5f49a09d38e
wizguin <16276645+wizguin@users.noreply.github.com>
2024-02-07 14:17:13 +0000

    pet: Add model

---
+++ src/database/models/Pets.js
@@ -0,0 +1,71 @@
+import BaseModel from '../BaseModel'
+
+import Sequelize from 'sequelize'
+
+
+export default class Pets extends BaseModel {
+
+    static init(sequelize, DataTypes) {
+        return super.init(
+            {
+                id: {
+                    type: DataTypes.INTEGER(11),
+                    allowNull: false,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                userId: {
+                    type: DataTypes.INTEGER(11),
+                    allowNull: false
+                },
+                petId: {
+                    type: DataTypes.INTEGER(11),
+                    allowNull: false
+                },
+                name: {
+                    type: DataTypes.STRING(12),
+                    allowNull: false,
+                },
+                adoptionDate: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: DataTypes.NOW
+                },
+                energy: {
+                    type: DataTypes.INTEGER(3),
+                    allowNull: false,
+                    defaultValue: 100
+                },
+                health: {
+                    type: DataTypes.INTEGER(3),
+                    allowNull: false,
+                    defaultValue: 100
+                },
+                rest: {
+                    type: DataTypes.INTEGER(3),
+                    allowNull: false,
+                    defaultValue: 100
+                }
+            },
+            { sequelize, timestamps: false, tableName: 'pets' }
+        )
+    }
+
+    static associate({ users }) {
+        this.belongsTo(users, {
+            foreignKey: 'userId'
+        })
+    }
+
+    toJSON() {
+        return pick(this,
+            'id',
+            'petId',
+            'name',
+            'energy',
+            'health',
+            'rest'
+        )
+    }
+
+}
