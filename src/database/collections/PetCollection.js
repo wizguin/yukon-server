8c9fa3254c1962bc9d5b683ea9a6d8d7df40f845
wizguin <16276645+wizguin@users.noreply.github.com>
2024-02-08 14:08:15 +0000

    pet: Add draining stats

--- src/database/collections/PetCollection.js
+++ src/database/collections/PetCollection.js
@@ -7,6 +7,12 @@ export default class PetCollection extends Collection {
         super(user, models, 'pets', 'id')
 
         this.adoptPostcard = 111
+
+        // 30 minutes
+        const updatePetsInterval = 30 * 60000
+
+        this.statLoss = 4
+        this.petUpdate = setInterval(() => this.updatePets(), updatePetsInterval)
     }
 
     async add(petId, name) {
@@ -19,6 +25,36 @@ export default class PetCollection extends Collection {
         } catch (error) {
             this.handler.error(error)
         }
+    }
+
+    updatePets() {
+        const updates = []
+
+        for (const pet of Object.values(this.collection)) {
+            pet.energy = this.getNewStat(pet.energy)
+            pet.health = this.getNewStat(pet.health)
+            pet.rest = this.getNewStat(pet.rest)
+
+            updates.push({
+                id: pet.id,
+                energy: pet.energy,
+                health: pet.health,
+                rest: pet.rest
+            })
+        }
+
+        if (updates.length) {
+            // Bulk update
+            this.model.bulkCreate(updates, { updateOnDuplicate: ['energy', 'health', 'rest'] })
+        }
+    }
+
+    getNewStat(stat) {
+        return Math.max(0, stat - this.statLoss)
+    }
+
+    stopPetUpdate() {
+        clearInterval(this.petUpdate)
     }
 
     toJSON() {
