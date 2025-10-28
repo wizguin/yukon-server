6291ff6ae1956f65d02ecfac67fcadc962ddb578
wizguin <16276645+wizguin@users.noreply.github.com>
2025-10-28 11:50:26 +0000

    Migrate data to ts

--- data/data.ts
+++ data/data.ts
@@ -1,15 +1,17 @@
-const fs = require('fs')
-const path = require('path')
+import fs from 'fs'
+import path from 'path'
 
 
 let files = fs.readdirSync(__dirname).filter(file => {
     return path.extname(file) == '.json'
 })
 
-const data = {}
+const data: Record<string, any> = {}
 
 for (let file of files) {
     data[file.replace('.json', '')] = require(`./${file}`)
 }
 
 export default data
+
+export const { cards, decks, floorings, furnitures, igloos, items, matchmakers, pets, rooms, tables, waddles } = data
