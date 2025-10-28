8013891dce03906b5a4a2ad511194916fa0c7849
wizguin <16276645+wizguin@users.noreply.github.com>
2025-10-28 11:55:04 +0000

    Add config types

---
+++ src/config/config.ts
@@ -0,0 +1,46 @@
+import type { Dialect } from 'sequelize'
+
+export interface Config {
+    crypto: {
+        secret: string
+        rounds: number
+        loginKeyExpiry: number
+    }
+    database: {
+        host: string
+        user: string
+        password: string
+        database: string
+        dialect: Dialect
+        debug: boolean
+        logQueryParameters: boolean
+    }
+    socketio: {
+        https: boolean
+        ssl: {
+            cert: string
+            ca: string
+            key: string
+        }
+    }
+    cors: {
+        origin: string
+    }
+    rateLimit: {
+        enabled: boolean
+        addressConnectsPerSecond: number
+        addressEventsPerSecond: number
+        userEventsPerSecond: number
+        ipAddressHeader: false | string
+    }
+    worlds: Record<string, {
+        host: string
+        port: number
+        maxUsers?: number
+    }>
+    cooldowns: Record<string, number>
+    game: {
+        preferredSpawn: number
+        iglooIdOffset: number
+    }
+}
