f05b02fa1ade8f04d44933fcc8370a443da5099a
wizguin <16276645+wizguin@users.noreply.github.com>
2025-10-29 20:42:57 +0000

    Migrate RateLimiter to ts

--- src/ratelimit/RateLimiter.ts
+++ src/ratelimit/RateLimiter.ts
@@ -1,15 +1,20 @@
+import type { Config } from '../config/config'
 import { RateLimiterMemory } from 'rate-limiter-flexible'
 
 
 export default class RateLimiter {
 
-    constructor(config) {
+    addressConnects: RateLimiterMemory
+    addressEvents: RateLimiterMemory
+    userEvents: RateLimiterMemory
+
+    constructor(config: Config) {
         this.addressConnects = this.createLimiter(config.rateLimit.addressConnectsPerSecond)
         this.addressEvents = this.createLimiter(config.rateLimit.addressEventsPerSecond)
         this.userEvents = this.createLimiter(config.rateLimit.userEventsPerSecond)
     }
 
-    createLimiter(points) {
+    createLimiter(points: number) {
         return new RateLimiterMemory({
             points: points,
             duration: 1
