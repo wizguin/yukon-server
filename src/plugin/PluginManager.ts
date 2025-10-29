92953c607bfe2f5297002fb162dafd72be32dfbc
wizguin <16276645+wizguin@users.noreply.github.com>
2025-10-29 20:45:27 +0000

    Migrate PluginManager to ts

--- src/plugin/PluginManager.ts
+++ src/plugin/PluginManager.ts
@@ -1,10 +1,19 @@
+import type BaseHandler from '../handlers/BaseHandler'
+import type Plugin from './Plugin'
+
+import type { EventEmitter } from 'stream'
 import fs from 'fs'
 import path from 'path'
 
 
 export default class PluginManager {
 
-    constructor(handler, pluginsDir) {
+    events: EventEmitter
+    id: string
+    dir: string
+    plugins: Record<string, Plugin> = {}
+
+    constructor(handler: BaseHandler, pluginsDir: string) {
         this.events = handler.events
         this.id = handler.id
 
@@ -14,7 +23,7 @@ export default class PluginManager {
         this.loadPlugins(handler)
     }
 
-    loadPlugins(handler) {
+    loadPlugins(handler: BaseHandler) {
         let plugins = fs.readdirSync(this.dir).filter(file => {
             return path.extname(file) == '.ts'
         })
@@ -29,12 +38,13 @@ export default class PluginManager {
         }
 
         let pluginsCount = Object.keys(this.plugins).length
+        // @ts-expect-error temp
         let eventsCount = this.events._eventsCount
 
         console.log(`[${this.id}] Loaded ${pluginsCount} plugins and ${eventsCount} events`)
     }
 
-    loadEvents(plugin) {
+    loadEvents(plugin: Plugin) {
         for (let event in plugin.events) {
             this.events.on(event, plugin.events[event].bind(plugin))
         }
