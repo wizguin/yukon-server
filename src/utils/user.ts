f2537d3ff3379417233768c608faccb7d90c8100
wizguin <16276645+wizguin@users.noreply.github.com>
2026-02-16 14:06:55 +0000

    Move user db utils to separate file

---
+++ src/utils/user.ts
@@ -0,0 +1,51 @@
+import type { UserSelect, UserWhereUniqueInput } from '../generated/prisma/models'
+import PrismaDatabase from '@database/PrismaDatabase'
+
+export async function getUserById(id: number) {
+    if (!id) {
+        return null
+    }
+
+    return getUser({ id })
+}
+
+export async function getUserByUsername(username: string) {
+    if (!username) {
+        return null
+    }
+
+    return getUser({ username })
+}
+
+export async function userIdExists(userId: number) {
+    if (!userId) {
+        return false
+    }
+
+    const user = await getUser({ id: userId }, { id: true })
+
+    return user !== null
+}
+
+export async function usernameExists(username: string) {
+    if (!username) {
+        return false
+    }
+
+    const user = await getUser({ username }, { id: true })
+
+    return user !== null
+}
+
+async function getUser(where: UserWhereUniqueInput, select?: UserSelect) {
+    return PrismaDatabase.user.findUnique({
+        where,
+        select
+    })
+}
+
+export async function getUsername(userId: number) {
+    const user = await getUser({ id: userId }, { username: true })
+
+    return user?.username ?? ''
+}
