09287c5b6fcd143e570b558e00304d9627b7d986
wizguin <16276645+wizguin@users.noreply.github.com>
2026-02-16 14:41:26 +0000

    Update Get to use prisma db

--- src/plugin/plugins/game/Get.ts
+++ src/plugin/plugins/game/Get.ts
@@ -3,8 +3,10 @@ import GamePlugin from '@plugin/GamePlugin'
 import type { Args } from '../../../server/Server'
 import type GameHandler from '../../../handlers/GameHandler'
 import type GameUser from '@objects/user/GameUser'
+import type { User } from '../../../generated/prisma/client'
 
 import { hasProps, isNumber } from '@utils/validation'
+import { getUserById } from '@utils/user'
 
 export default class Get extends GamePlugin {
 
@@ -26,16 +28,33 @@ export default class Get extends GamePlugin {
         }
 
         if (args.id in this.usersById) {
-            return user.send('get_player', { penguin: this.usersById[args.id].anonymous })
+            return user.send('get_player', { penguin: this.formatGetPlayer(this.usersById[args.id]) })
         }
 
         if (!user.buddies.includes(args.id)) {
             return
         }
 
-        const u = await this.db.getUserById(args.id)
+        const u = await getUserById(args.id)
+
         if (u) {
-            user.send('get_player', { penguin: u.anonymous })
+            user.send('get_player', { penguin: this.formatGetPlayer(u) })
+        }
+    }
+
+    formatGetPlayer({ id, username, head, face, neck, body, hand, feet, color, photo, flag }: Partial<User>) {
+        return {
+            id,
+            username,
+            head,
+            face,
+            neck,
+            body,
+            hand,
+            feet,
+            color,
+            photo,
+            flag
         }
     }
 
