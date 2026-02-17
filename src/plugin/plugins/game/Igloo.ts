71313c1b6384baebdf55f764aa3ad5a8fc81c7eb
wizguin <16276645+wizguin@users.noreply.github.com>
2026-02-17 22:32:24 +0000

    Update updateFurniture to use prisma db

--- src/plugin/plugins/game/Igloo.ts
+++ src/plugin/plugins/game/Igloo.ts
@@ -5,6 +5,7 @@ import { config } from '@config'
 import type GameHandler from '../../../handlers/GameHandler'
 import type GameUser from '@objects/user/GameUser'
 import type IglooRoom from '@objects/room/Igloo'
+import PrismaDatabase from '@database/PrismaDatabase'
 
 import { isInRange } from '@utils/validation'
 
@@ -111,8 +112,9 @@ export default class Igloo extends GamePlugin {
             igloo.furniture.push({ ...item, userId: user.id })
         }
 
-        // @ts-expect-error temp
-        this.db.furnitures.bulkCreate(igloo.furniture)
+        await PrismaDatabase.furniture.createMany({
+            data: igloo.furniture
+        })
     }
 
     updateFlooring(args: Args, user: GameUser) {
